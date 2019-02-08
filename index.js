4d4fce6b1e4624601aa32aef51732f4d165c52c7
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-08 15:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mixedCase.toLowerCase() === mixedCase // false
  
 mixedCase.toUpperCase() === mixedCase // false
 
-function sayHiToGrandma(string) {if if (string==="I love you, Grandma.") {return "I love you, too."} 
+function sayHiToGrandma(string) {if (string==="I love you, Grandma.") {return "I love you, too."} 
 else if (string === lowercase){return "I can\'t hear you!"} 
 else {return "YES INDEED!"} }
 
