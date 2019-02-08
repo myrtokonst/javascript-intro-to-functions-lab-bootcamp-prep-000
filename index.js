becef17a6a3e6df83455bd2657bbdc05f04ae2f5
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-08 15:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) { console.log(string.toUpperCase())}
 function logWhisper(string) { console.log(string.toLowerCase())}
-var uppercase = "HELLO!"
+var uppercase = "HELLO"
  
 uppercase.toUpperCase() === uppercase // true
  
-var lowercase = 'hello!'
+var lowercase = 'hello'
  
 lowercase.toLowerCase() === lowercase // true
  
-var mixedCase = 'Hi there!'
+var mixedCase = 'Hi there'
  
 mixedCase.toLowerCase() === mixedCase // false
  
